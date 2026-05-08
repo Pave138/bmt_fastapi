00fbdc567e3cf7765b1ff6798588817bbb219019
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-08 16:54:08 +0800

    Update Category model and create migration.

--- src/app/models/category.py
+++ src/app/models/category.py
@@ -1,20 +1,30 @@
+from typing import Optional
+
 from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.models.common import Base, CommonMixin
 
-
+## Slug field in future.
 class Category(CommonMixin, Base):
     name: Mapped[str] = mapped_column(
         String(length=255),
         nullable=False,
-        index=True
+        index=True,
+        unique=True
     )
     parent_id: Mapped[int] = mapped_column(
         Integer,
-        ForeignKey('category.id'),
-        nullable=True
+        ForeignKey('category.id', ondelete='SET NULL'),
+        nullable=True,
+        index=True
     )
-    parent = relationship(
+    parent: Mapped[Optional['Category']] = relationship(
         'Category',
         remote_side='Category.id',
-        back_populates='children')
+        back_populates='children'
+    )
+    children: Mapped[list['Category']] = relationship(
+        'Category',
+        back_populates='parent',
+        cascade='all, delete-orphan'
+    )
