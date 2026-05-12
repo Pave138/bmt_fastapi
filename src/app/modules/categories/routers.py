a7cbf2759f059acabc24e42b568fc03561310fed
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-12 14:16:13 +0800

    Add Category update endpoint.

--- src/app/modules/categories/routers.py
+++ src/app/modules/categories/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from app.modules.auth.dependencies import current_superuser
 from app.modules.categories.schemas import (
-    CategoryCreate, CategoryRead
+    CategoryCreate, CategoryRead, CategoryUpdate
 )
 from app.modules.categories.dependencies import CategoryServiceDep
 from app.modules.categories.models import Category
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.post(
     '/',
     response_model=CategoryRead,
-    summary='Создать категорию',
+    summary='Создать категорию (superuser only)',
     description='Создает новую категорию',
     dependencies=[Depends(current_superuser)]
 )
@@ -40,5 +40,20 @@ async def get_all(service: CategoryServiceDep) -> list[Category]:
     summary='Получить категорию по ID',
     description='Получает категорию по ID'
 )
-async def get_by_id(service: CategoryServiceDep, category_id: int):
+async def get_by_id(category_id: int, service: CategoryServiceDep) -> Category:
     return await service.get_by_id(category_id)
+
+
+@router.patch(
+    '/{category_id}/edit',
+    response_model=CategoryRead,
+    summary='Изменить категорию по ID (superuser only)',
+    description='Изменяет категорию по ID',
+    dependencies=[Depends(current_superuser)]
+)
+async def update(
+    category_id: int,
+    data: CategoryUpdate,
+    service: CategoryServiceDep
+) -> Category:
+    return await service.update(category_id, data)
