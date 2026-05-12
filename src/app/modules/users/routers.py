241cfe3d0b7c61193190b73cf8c53b38b2a4003b
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-12 20:29:50 +0800

    Exclude users:delete_user router.

--- src/app/modules/users/routers.py
+++ src/app/modules/users/routers.py
@@ -5,12 +5,9 @@ from .schemas import UserRead, UserUpdate
 
 router = APIRouter()
 
+users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
+users_router.routes = [
+    route for route in users_router.routes if route.name != 'users:delete_user'
+]
 
-router.include_router(
-    fastapi_users.get_users_router(
-        UserRead,
-        UserUpdate,
-    ),
-    prefix="",
-    tags=["users"],
-)
+router.include_router(users_router)
