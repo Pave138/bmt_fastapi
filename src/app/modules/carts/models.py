d3b535edf6fc7c05e2d64fef95b395538a2b63ba
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-14 18:11:04 +0800

    Add CartItem model.

--- src/app/modules/carts/models.py
+++ src/app/modules/carts/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, CheckConstraint, Index
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
@@ -12,3 +12,31 @@ class Cart(CommonMixin, Base):
         nullable=False
     )
     user: Mapped['User'] = relationship('User', back_populates='cart')
+
+
+class CartItem(CommonMixin, TimestampMixin, Base):
+    cart_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('cart.id', ondelete='CASCADE')
+    )
+    product_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('product.id', ondelete='RESTRICT')
+    )
+    quantity: Mapped[int] = mapped_column(
+        Integer,
+        default=1,
+        server_default='1',
+        nullable=False
+    )
+
+    cart: Mapped['Cart'] = relationship('Cart', back_populates='items')
+    product: Mapped['Product'] = relationship('Product')
+
+    __table_args__ = (
+        Index('ix_cart_product_unique', 'cart_id', 'product_id', unique=True),
+        CheckConstraint(
+            'quantity > 0',
+            name='ck_cart_item_quantity_positive'
+        )
+    )
