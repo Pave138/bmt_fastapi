d0d8df675a6aeff308009524898a3b58c5b3f681
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-19 16:00:56 +0800

    Add Review model import to models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,6 +1,7 @@
-from app.db.base import Base # noqa
-from app.modules.users.models import User # noqa
-from app.modules.categories.models import Category # noqa
-from app.modules.products.models import Product, ProductImage # noqa
-from app.modules.carts.models import Cart # noqa
-from app.modules.cart_items.models import CartItem # noqa
+from app.db.base import Base  # noqa
+from app.modules.users.models import User  # noqa
+from app.modules.categories.models import Category  # noqa
+from app.modules.products.models import Product, ProductImage  # noqa
+from app.modules.carts.models import Cart  # noqa
+from app.modules.cart_items.models import CartItem  # noqa
+from app.modules.reviews.models import Review  # noqa
