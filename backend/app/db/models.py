d9d168af7bd4c29da40a84e6e8173a275dfc3d0e
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-19 17:28:30 +0800

    Add OrderItem model import to models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -6,3 +6,4 @@ from app.modules.carts.models import Cart  # noqa
 from app.modules.cart_items.models import CartItem  # noqa
 from app.modules.reviews.models import Review  # noqa
 from app.modules.orders.models import Order  # noqa
+from app.modules.order_items.models import OrderItem  # noqa
