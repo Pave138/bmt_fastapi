bd958e75b23a10cf7b436639abf3cde787044f95
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-20 00:23:07 +0800

    Include cart_router to main_router.

--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -1,20 +1,38 @@
 from fastapi import APIRouter
 
 from app.modules.auth.routers import router as auth_router
+from app.modules.carts.routers import router as cart_router
 from app.modules.categories.routers import router as category_router
 from app.modules.products.routers import router as product_router
 from app.modules.users.routers import router as user_router
 
 main_router = APIRouter()
-main_router.include_router(auth_router, prefix='/auth')
-main_router.include_router(user_router, prefix='/users', tags=['Пользователи'])
+
+main_router.include_router(
+    auth_router,
+    prefix='/auth'
+)
+
+main_router.include_router(
+    user_router,
+    prefix='/users',
+    tags=['Пользователи']
+)
+
 main_router.include_router(
     category_router,
     prefix='/categories',
     tags=['Категории']
 )
+
 main_router.include_router(
     product_router,
     prefix='/products',
     tags=['Товары']
+)
+
+main_router.include_router(
+    cart_router,
+    prefix='/carts',
+    tags=['Корзины']
 )
