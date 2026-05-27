4db42d84220fb4f69bc959f04c6435a695ad0847
Pavel Protopopov <p0chta3@yandex.ru>
2026-05-27 18:40:30 +0800

    Add new keys.

--- backend/app/services/cache/keys.py
+++ backend/app/services/cache/keys.py
@@ -6,10 +6,18 @@ def get_products_key(limit: int, offset: int) -> str:
     return f'products:{limit}:{offset}'
 
 
-def get_category_key(
+def get_category_products_key(
         category_id: int,
         limit: int,
         offset: int
 ) -> str:
     return (f'products:category:{category_id}:'
             f'limit:{limit}:offset:{offset}')
+
+
+def get_category_key(category_id) -> str:
+    return f'category:{category_id}'
+
+
+def get_categories_key() -> str:
+    return'categories'
